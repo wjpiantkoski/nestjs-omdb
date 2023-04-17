028081843782a0a0bff19f6b707903f53d832f20
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-17 10:08:30 -0300

    feat: add method getMovies in omdb service

--- src/omdb/omdb.service.ts
+++ src/omdb/omdb.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import { OmdbMovieDto } from "./dtos/omdb-movie.dto";
 
 @Injectable()
-export class OmdbService {}
+export class OmdbService {
+
+  private apiBase: string = process.env.OMDB_API_BASE;
+  private apiKey: string = process.env.OMDB_API_KEY;
+
+  constructor(private readonly httpService: HttpService) {
+  }
+
+  async getMovies(title: string): Promise<OmdbMovieDto> {
+    const url = `${this.apiBase}?apikey=${this.apiKey}&t=${title}`;
+    const { data } = await this.httpService.axiosRef.get(url);
+
+    return {
+      imdbID: data.imdbID,
+      Title: data.Title,
+      Actors: data.Actors,
+      Plot: data.Plot,
+      Poster: data.Poster
+    };
+  }
+}
