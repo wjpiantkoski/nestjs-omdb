88ba140df63ede4629d71ea228bf0e6f2c60990b
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-17 12:55:44 -0300

    feat: add endpoint GET /movies/favorites

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -3,6 +3,7 @@ import { SearchMoviesDto } from "./dtos/search-movies.dto";
 import { OmdbService } from "../omdb/omdb.service";
 import { CreateFavoriteDto } from "./dtos/create-favorite.dto";
 import { MoviesService } from "./movies.service";
+import { ListFavoritesDto } from "./dtos/list-favorites.dto";
 
 @Controller('movies')
 export class MoviesController {
@@ -20,6 +21,17 @@ export class MoviesController {
     }
 
     return this.omdbService.getMovie(query.title)
+  }
+
+  @Get('/favorites')
+  getFavoritesMovies(@Query() query: ListFavoritesDto) {
+    let {page, limit} = query
+
+    const pageNumber = page ? parseInt(page) : 1
+    const limitNumber = limit ? parseInt(limit) : 10
+    const skip = (pageNumber - 1) * limitNumber
+
+    return this.moviesService.findAll(skip, limitNumber)
   }
 
   @Post('/favorites')
