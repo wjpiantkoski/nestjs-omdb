d13d0c39a3870d32f48a363f4a91df30b4547fcf
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-17 17:01:43 -0300

    feat: generate access token on user's sign in

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,27 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "./entities/user";
+import { ConfigService } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User])
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        return {
+          global: true,
+          secret: config.get<string>('JWT_SECRET'),
+          signOptions: { expiresIn: "1h" }
+        };
+      }
+    })
   ],
   controllers: [UsersController],
   providers: [UsersService]
 })
-export class UsersModule {}
+export class UsersModule {
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,46 +2,53 @@ import { BadRequestException, Injectable, UnauthorizedException } from "@nestjs/
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "./entities/user";
 import { Repository } from "typeorm";
-import {randomBytes, scryptSync} from 'crypto'
+import { randomBytes, scryptSync } from "crypto";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class UsersService {
 
-  constructor(@InjectRepository(User) private repository: Repository<User>) {
+  constructor(
+    @InjectRepository(User) private repository: Repository<User>,
+    private jwtService: JwtService
+  ) {
   }
 
   async createUser(email: string, password: string) {
-    const foundUser = await this.repository.findOneBy({ email })
+    const foundUser = await this.repository.findOneBy({ email });
 
     if (foundUser) {
-      throw new BadRequestException('E-mail already used')
+      throw new BadRequestException("E-mail already used");
     }
 
-    const salt = randomBytes(8).toString('hex')
-    const hash = (await scryptSync(password, salt, 32)) as Buffer
-    const result = salt + '.' + hash.toString('hex')
+    const salt = randomBytes(8).toString("hex");
+    const hash = (await scryptSync(password, salt, 32)) as Buffer;
+    const result = salt + "." + hash.toString("hex");
 
     const user = this.repository.create({
       email,
       password: result
-    })
+    });
 
-    return this.repository.save(user)
+    return this.repository.save(user);
   }
 
   async signIn(email: string, password: string) {
-    const user = await this.repository.findOneBy({ email })
+    const user = await this.repository.findOneBy({ email });
 
     if (!user) {
-      throw new UnauthorizedException()
+      throw new UnauthorizedException();
     }
 
-    const [salt, storedHash] = user.password.split('.')
-    const hash = (await scryptSync(password, salt, 32)) as Buffer
+    const [salt, storedHash] = user.password.split(".");
+    const hash = (await scryptSync(password, salt, 32)) as Buffer;
 
-    if (hash.toString('hex') !== storedHash) {
-      throw new UnauthorizedException()
+    if (hash.toString("hex") !== storedHash) {
+      throw new UnauthorizedException();
     }
+
+    const payload = { email: user.email, id: user.id }
+    user['access_token'] = await this.jwtService.signAsync(payload)
 
     return user
   }
