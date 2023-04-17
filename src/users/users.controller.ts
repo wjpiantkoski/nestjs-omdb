a72cb1adfb2fb096127aee3b02d071d0a973fa4b
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-17 16:34:10 -0300

    feat: add user's signup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from "@nestjs/common";
+import { UserSignupDto } from "./dtos/user-signup.dto";
+import { UsersService } from "./users.service";
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+  constructor(private usersService: UsersService) {
+  }
+
+  @Post('/signup')
+  async signUpUser(@Body() body: UserSignupDto) {
+    await this.usersService.createUser(body.email, body.password)
+  }
+
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { User } from "./entities/user";
+import { Repository } from "typeorm";
+import {randomBytes, scryptSync} from 'crypto'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+  constructor(@InjectRepository(User) private repository: Repository<User>) {
+  }
+
+  async createUser(email: string, password: string) {
+    const foundUser = await this.repository.findOneBy({ email })
+
+    if (foundUser) {
+      throw new BadRequestException('E-mail already used')
+    }
+
+    const salt = randomBytes(8).toString('hex')
+    const hash = (await scryptSync(password, salt, 32)) as Buffer
+    const result = salt + '.' + hash.toString('hex')
+
+    const user = this.repository.create({
+      email,
+      password: result
+    })
+
+    return this.repository.save(user)
+  }
+
+}
