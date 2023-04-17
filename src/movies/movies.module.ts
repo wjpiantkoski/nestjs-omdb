c0a43e8cbcd927b5aa2b185a0d7be271630888a9
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-17 11:23:42 -0300

    feat: add FavoriteMovie mongoose Schema

--- src/movies/movies.module.ts
+++ src/movies/movies.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { MoviesController } from './movies.controller';
 import { MoviesService } from './movies.service';
 import { OmdbModule } from "../omdb/omdb.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { FavoriteMovie, FavoriteMovieSchema } from "./schemas/favorite-movie.schema";
 
 @Module({
   imports: [
-    OmdbModule
+    OmdbModule,
+    MongooseModule.forFeature([
+      {
+        name: FavoriteMovie.name,
+        schema: FavoriteMovieSchema
+      }
+    ])
   ],
   controllers: [
     MoviesController
