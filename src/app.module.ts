8ff6023d4d9288ef050ce0192019780bb756d089
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-17 11:38:06 -0300

    chore: add validation pipe provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
-import { Module } from "@nestjs/common";
+import { Module, ValidationPipe } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { OmdbModule } from './omdb/omdb.module';
 import { MoviesModule } from './movies/movies.module';
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { MongooseModule } from '@nestjs/mongoose'
+import { APP_PIPE } from "@nestjs/core";
 
 @Module({
   imports: [
@@ -24,7 +25,15 @@ import { MongooseModule } from '@nestjs/mongoose'
     MoviesModule
   ],
   controllers: [AppController],
-  providers: [AppService]
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true
+      })
+    }
+  ]
 })
 export class AppModule {
 }
