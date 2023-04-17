beb35ace3cd5e8f7633265788c914fc77f7530e2
Willian Piantkoski <wjpiantkoski@gmail.com>
2023-04-17 11:39:13 -0300

    feat: add method createFavoriteMovie into movies service

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { CreateFavoriteDto } from "./dtos/create-favorite.dto";
+import { InjectModel } from "@nestjs/mongoose";
+import { FavoriteMovie } from "./schemas/favorite-movie.schema";
+import { Model } from "mongoose";
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+
+  constructor(@InjectModel(FavoriteMovie.name) private favoriteMovieModel: Model<FavoriteMovie>) {
+  }
+
+  async createFavoriteMovie(data: CreateFavoriteDto) {
+    const foundFavoriteMovie = await this.favoriteMovieModel.findOne({ imdbID: data.imdbID })
+
+    if (foundFavoriteMovie) {
+      return foundFavoriteMovie
+    }
+
+    const favoriteMovie = new this.favoriteMovieModel(data)
+    return favoriteMovie.save()
+  }
+
+}
